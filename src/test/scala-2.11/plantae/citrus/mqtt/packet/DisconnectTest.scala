7a54d89e181359ee99e25d07225ebcaffc56a8cc
wonsuk.yang <before30@gmail.com>
2015-05-02 01:12:56 +0900

    * complete disconnect test

---
+++ src/test/scala-2.11/plantae/citrus/mqtt/packet/DisconnectTest.scala
@@ -0,0 +1,19 @@
+package plantae.citrus.mqtt.packet
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import scodec.{DecodeResult, Codec}
+import scodec.bits._
+
+@RunWith(classOf[JUnitRunner])
+class DisconnectTest extends FunSuite {
+  test("encode/decode Disconnect") {
+    val fh = FixedHeader()
+    val disconnect = DisconnectPacket(fh)
+
+    assert(Codec[Packet].encode(disconnect).require.bytes.size === 2)
+    assert(Codec[Packet].decode(Codec[Packet].encode(disconnect).require).require === DecodeResult(disconnect, bin""))
+  }
+
+}
